e64845a5cef9ca3658fcfc714a340190d9573fd1
TimIsOverpowered <admin@angelthump.com>
2017-05-26 22:49:37 +0000

    clean up

--- src/services/user/hooks/insensitive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function() {
-  return function(hook) {
-    if(typeof hook.params.query.username !== 'undefined') {
-    	hook.params.query.username = hook.params.query.username.toLowerCase();
-    }
-  };
-};
