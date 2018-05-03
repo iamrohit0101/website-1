ba96a12e6c694e82f3d019963976c9fb27096097
TimIsOverpowered <admin@angelthump.com>
2018-05-03 08:10:28 -0500

    final pls

--- src/middleware/embed.js
+++ src/middleware/embed.js
@@ -36,9 +36,9 @@ module.exports = function(app) {
                                     var servers = [];
 
                                     if(continent == 'NA') {
-                                        servers = ["https://nyc1.angelthump.com/", "https://sfo1.angelthump.com/"]
+                                        servers = ["https://nyc1.angelthump.com/", "https://sfo1.angelthump.com/", "https://tor1.angelthump.com"]
                                     } else if (continent == 'SA') {
-                                        servers = ["https://nyc1.angelthump.com/", "https://sfo1.angelthump.com/"]
+                                        servers = ["https://nyc1.angelthump.com/", "https://sfo1.angelthump.com/", "https://tor1.angelthump.com"]
                                     } else if (continent == 'EU') { //frankfurt london amsterdam 
                                         servers = ["https://fra1.angelthump.com/", "https://lon1.angelthump.com/", "https://ams1.angelthump.com/"]
                                     } else if (continent == 'AS') { //singapore, bangalore
@@ -65,9 +65,9 @@ module.exports = function(app) {
                             var servers = [];
 
                             if(continent == 'NA') {
-                                servers = ["https://nyc1.angelthump.com/", "https://sfo1.angelthump.com/"]
+                                servers = ["https://nyc1.angelthump.com/", "https://sfo1.angelthump.com/", "https://tor1.angelthump.com"]
                             } else if (continent == 'SA') {
-                                servers = ["https://nyc1.angelthump.com/", "https://sfo1.angelthump.com/"]
+                                servers = ["https://nyc1.angelthump.com/", "https://sfo1.angelthump.com/", "https://tor1.angelthump.com"]
                             } else if (continent == 'EU') { //frankfurt london amsterdam 
                                 servers = ["https://fra1.angelthump.com/", "https://lon1.angelthump.com/", "https://ams1.angelthump.com/"]
                             } else if (continent == 'AS') { //singapore, bangalore
